0f80a09eb62dc92159c5f4f015e676b1f971d7b3
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-08-18 22:21:39 +0300

    198. Revealing Elements on Scroll

--- script.js
+++ script.js
@@ -290,3 +290,31 @@ const headerObserver = new IntersectionObserver(
     rootMargin: `-${navHeight}px` }
 );
 headerObserver.observe(header);
+
+
+//  198. Revealing Elements on Scroll
+
+const allSections = document.querySelectorAll('.section');
+
+function revealSection(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    return;
+  }
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(
+  revealSection,
+  { 
+    root: null, 
+    threshold: 0.15,
+  }
+);
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
